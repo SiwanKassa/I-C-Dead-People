30c7a185652efdb5deeb8e3f5bcf4aa68972a9f7
OskariV <34027142+OskariV@users.noreply.github.com>
2018-04-20 10:48:14 +0300

    E-sports ready

--- main.c
+++ main.c
@@ -199,56 +199,136 @@ int main()
 {
     struct sensors_ ref;
     struct sensors_ dig;
+    int firstLine = 0;
+    int firstLinePassed =0;
+    int secondLine = 0;
+    int secondLinePassed =0;
+    int outOfBounds = 0;
+    bool StartSignal = true;
 
     Systick_Start();
 
     CyGlobalIntEnable; 
     UART_1_Start();
+    IR_Start();
   
     reflectance_start();
+    CyDelay(2);
     reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center sensor threshold to 11000 and others to 9000
     motor_start();
     //motor_forward(0,0);
     MotorDirLeft_Write (0);
     MotorDirRight_Write (0);
+    
+    //Robot reaches starting line and stops
+    while(StartSignal == true)
+    {
+        reflectance_read(&ref);
+        reflectance_digital(&dig);
+        // Stops robot at starting line
+        if (dig.l1 == 1 && dig.l2 == 1 && dig.l3 == 1 && dig.r1 == 1 && dig.r2 == 1 && dig.r3 == 1){
+            motor_forward (0,0);
+            IR_wait();
+            StartSignal = false;
+            motor_forward(75,100);
+        }// Moves robot forward until it reaches the startingline
+        else {
+            motor_forward (75,1);
+        }
+    }
+    ShieldLed_Write (0);
 
     for(;;)
     {
         // read raw sensor values
         reflectance_read(&ref);
-        printf("%5d %5d %5d %5d %5d %5d\r\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       // print out each period of reflectance sensors
+        //printf("%5d %5d %5d %5d %5d %5d\r\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       // print out each period of reflectance sensors
         
         // read digital values that are based on threshold. 0 = white, 1 = black
         // when blackness value is over threshold the sensors reads 1, otherwise 0
         reflectance_digital(&dig);      //print out 0 or 1 according to results of reflectance period
-        printf("%5d %5d %5d %5d %5d %5d \r\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);        //print out 0 or 1 according to results of reflectance period
+        //printf("%5d %5d %5d %5d %5d %5d \r\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);        //print out 0 or 1 according to results of reflectance period
+        BatteryLed_Write (0);
         
-        CyDelay(3);
-        //{1 1 1 1 1 1} stop
+        
+        //{1 1 1 1 1 1} stop : supposed to stop if has encountered 3 black lines
         if (dig.l1 == 1 && dig.l2 == 1 && dig.l3 == 1 && dig.r1 == 1 && dig.r2 == 1 && dig.r3 == 1){
-            motor_forward(0,0);
+            firstLine++;
+            if (firstLine >= 1 && firstLinePassed >= 1){
+                secondLine++;
+            }
+            if (secondLinePassed >= 1){
+                motor_stop();
+                ShieldLed_Write (1);
+            }
+            
         }
-        //{0 0 1 1 0 0} forward 
+        //{0 0 0 0 0 0} stop or turn if track is lost depending on direction of travel
+        else if (dig.l1 == 0 && dig.l2 == 0 && dig.l3 == 0 && dig.r1 == 0 && dig.r2 == 0 && dig.r3 == 0){
+            if (outOfBounds == 1){
+                motor_turnLeft(150,2);
+                BatteryLed_Write (1);
+            }
+            if (outOfBounds == 2){
+                motor_turnRight(150,2);
+                BatteryLed_Write (1);
+            }
+            if (outOfBounds == 0) {
+                motor_forward (0,0);
+                BatteryLed_Write (1);
+            }
+        }
+        
+        //{0 0 1 1 0 0} forward : also checks how many lines have been passed
         else if (dig.l1 == 1 && dig.r1 == 1){
-            motor_forward(100,1);
+            motor_forward(250,2);
+           
         }
-        //{0 1 1 0 0 0} left  
+        //{0 1 1 0 0 0} soft left  
         else if (dig.l2 == 1 && dig.l1 == 1){
-            motor_turn(25,100,1);
+            motor_turn(150,250,2);
+            outOfBounds = 1;
         }
-        //{0 0 0 1 1 0} right 
+        //{0 0 0 1 1 0} soft right 
         else if (dig.r2 == 1 && dig.r1 == 1){
-            motor_turn(100,25,1);
+            motor_turn(250,150,2);
+            outOfBounds = 2;
+        }
+        //{1 1 0 0 0 0} left  
+        else if (dig.l3 == 1 && dig.l2 == 1){
+            motor_turn(80,210,2);
+            outOfBounds = 1;
+        }
+        //{0 0 0 0 1 1} right 
+        else if (dig.r3 == 1 && dig.r2 == 1){
+            motor_turn(210,80,2);
+            outOfBounds = 2;
         }
         //{0 0 0 0 0 1} hard left 
         else if (dig.l3 == 1){
-            motor_turnLeft(100,1);
+            //motor_turnLeft(150,1);
+            motor_turn (1,250,2);
+            outOfBounds = 1;
         }
         //{1 0 0 0 0 0} hard right
         else if(dig.r3 == 1){
-            motor_turnRight(100,1);
+            //motor_turnRight(150,1);
+            motor_turn (250,1,2);
+            outOfBounds = 2;
         }
-        
+        // Uses the sidemost sensor to check if line has been passed
+        if (dig.r3 == 0 && dig.l3)
+        {
+            if (firstLine >= 1){
+                ShieldLed_Write (1);
+                firstLinePassed++;
+            }
+            if (secondLine >= 1) {
+                ShieldLed_Write (0);
+                secondLinePassed++;
+            } 
+        }
+      CyDelay(1);  
     }
     
 }   
